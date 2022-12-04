3830c5cd241640230d63c47844aca7c879d48a61
Peter Weightman <peter.w570@gmail.com>
2022-12-04 12:05:13 +0000

    04

---
+++ 2022/04/part-01.ts
@@ -0,0 +1,20 @@
+const input = await Deno.readTextFile("input.txt");
+const lines = input.split("\n");
+
+const pointWithinRangeInclusive = (point: number, rangeMin: number, rangeMax: number) => point >= rangeMin && point <= rangeMax;
+
+const eitherRangeFullyContainedInOther = ([xMin, xMax]: number[], [yMin, yMax]: number[]) =>
+  (pointWithinRangeInclusive(xMin, yMin, yMax) && pointWithinRangeInclusive(xMax, yMin, yMax))
+  || (pointWithinRangeInclusive(yMin, xMin, xMax) && pointWithinRangeInclusive(yMax, xMin, xMax));
+
+let total = 0;
+for (const line of lines) {
+  const [elfOne, elfTwo] = line.split(",");
+  const elfOneRange = elfOne.split("-").map(str => Number(str));
+  const elfTwoRange = elfTwo.split("-").map(str => Number(str));
+
+  if (eitherRangeFullyContainedInOther(elfOneRange, elfTwoRange)) {
+    total++;
+  }
+}
+console.log(total);
