6f96e37f9155e81520a99c9d3c337c76021611ce
Peter Weightman <peter@peterweightman.com>
2023-12-01 14:24:32 +0000

    2023 day 01 golang part 2

--- 2023/01/go/main.go
+++ 2023/01/go/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -19,23 +20,99 @@ func main() {
 
 	fileScanner.Split(bufio.ScanLines)
 
-	var partOneTotal = 0
+	var partTwoTotal = 0
 
 	for fileScanner.Scan() {
-		var line = fileScanner.Text()
-		var lineDigits []int
+		var first int
+		var last int
 
-		for _, char := range line {
-			maybeParsed, err := strconv.Atoi(string(char))
+		var line = fileScanner.Text()
+		var toParse = line
+		for {
+			if len(toParse) == 0 {
+				break
+			}
+
+			maybeParsed, err := strconv.Atoi(string(toParse[0]))
 			if err == nil {
-				lineDigits = append(lineDigits, maybeParsed)
+				first = maybeParsed
+				break
+			} else if strings.HasPrefix(toParse, "one") {
+				first = 1
+				break
+			} else if strings.HasPrefix(toParse, "two") {
+				first = 2
+				break
+			} else if strings.HasPrefix(toParse, "three") {
+				first = 3
+				break
+			} else if strings.HasPrefix(toParse, "four") {
+				first = 4
+				break
+			} else if strings.HasPrefix(toParse, "five") {
+				first = 5
+				break
+			} else if strings.HasPrefix(toParse, "six") {
+				first = 6
+				break
+			} else if strings.HasPrefix(toParse, "seven") {
+				first = 7
+				break
+			} else if strings.HasPrefix(toParse, "eight") {
+				first = 8
+				break
+			} else if strings.HasPrefix(toParse, "nine") {
+				first = 9
+				break
+			} else {
+				toParse = toParse[1:]
 			}
 		}
 
-		var first = lineDigits[0]
-		var last = lineDigits[len(lineDigits)-1]
-		partOneTotal += (first * 10) + last
+		toParse = line
+		for {
+			if len(toParse) == 0 {
+				break
+			}
+
+			maybeParsed, err := strconv.Atoi(string(toParse[len(toParse)-1]))
+			if err == nil {
+				last = maybeParsed
+				break
+			} else if strings.HasSuffix(toParse, "one") {
+				last = 1
+				break
+			} else if strings.HasSuffix(toParse, "two") {
+				last = 2
+				break
+			} else if strings.HasSuffix(toParse, "three") {
+				last = 3
+				break
+			} else if strings.HasSuffix(toParse, "four") {
+				last = 4
+				break
+			} else if strings.HasSuffix(toParse, "five") {
+				last = 5
+				break
+			} else if strings.HasSuffix(toParse, "six") {
+				last = 6
+				break
+			} else if strings.HasSuffix(toParse, "seven") {
+				last = 7
+				break
+			} else if strings.HasSuffix(toParse, "eight") {
+				last = 8
+				break
+			} else if strings.HasSuffix(toParse, "nine") {
+				last = 9
+				break
+			} else {
+				toParse = toParse[:len(toParse)-1]
+			}
+		}
+
+		partTwoTotal += (first * 10) + last
 	}
 
-	fmt.Printf("Part 1: %d\n", partOneTotal)
+	fmt.Printf("Part 2: %d\n", partTwoTotal)
 }
