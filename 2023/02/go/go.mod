71ff92279ba9514084da07231530a3d6e08ebffb
Peter Weightman <peter@peterweightman.com>
2023-12-02 12:20:20 +0000

    2023 day 02 golang part 1

---
+++ 2023/02/go/go.mod
@@ -0,0 +1,3 @@
+module peterweightman.com/aoc-2023-02
+
+go 1.21.3
---
+++ 2023/02/go/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	readFile, err := os.Open("../input.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	partOneTotal := 0
+
+	for fileScanner.Scan() {
+		var line = fileScanner.Text()
+		var parsed = parseLine(line)
+		if parsed.maxCounts["red"] <= 12 && parsed.maxCounts["green"] <= 13 && parsed.maxCounts["blue"] <= 14 {
+			partOneTotal += parsed.gameNumber
+		}
+	}
+
+	fmt.Printf("Part 1: %d\n", partOneTotal)
+}
+
+type ParsedLine struct {
+	gameNumber int
+	maxCounts  map[string]int
+}
+
+func parseLine(line string) ParsedLine {
+	var parsedLine ParsedLine
+
+	gameSplits := strings.Split(line, ":")
+	gameStr := gameSplits[0]
+	shownCubesStr := gameSplits[1]
+
+	gameNum, err := strconv.Atoi(gameStr[len("Game "):])
+	if err != nil {
+		panic(err)
+	}
+	parsedLine.gameNumber = gameNum
+	parsedLine.maxCounts = make(map[string]int, 3)
+
+	roundStrs := strings.Split(shownCubesStr, ";")
+	for _, roundStr := range roundStrs {
+		re := regexp.MustCompile(`(\d+) (\w+)`)
+		matches := re.FindAllStringSubmatch(roundStr, -1)
+		for _, match := range matches {
+			count, err := strconv.Atoi(match[1])
+			if err != nil {
+				panic(err)
+			}
+			color := match[2]
+			parsedLine.maxCounts[color] = max(parsedLine.maxCounts[color], count)
+		}
+	}
+
+	return parsedLine
+}
