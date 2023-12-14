8a7a562aa658ed1317e0df0e4205528afebe4c77
Peter Weightman <peter@peterweightman.com>
2023-12-14 21:53:14 +0000

    2023 day 14 golang part 2

--- 2023/14/go/main.go
+++ 2023/14/go/main.go
@@ -16,6 +16,11 @@ const (
 
 var debugging = false
 
+type history struct {
+	load  int
+	state string
+}
+
 func main() {
 	grid := make([][]cellType, 0)
 	aoc_utils.IterateFileLines("../input.txt", func(line string) {
@@ -34,18 +39,46 @@ func main() {
 		grid = append(grid, gridLine)
 	})
 
-	tilted := tiltUp(grid)
-	partOneTotal := calculateLoad(tilted)
+	cycles := 1000000000
+	tilted := grid
+	loadHistory := make([]history, 0)
+	var partTwoTotal int
 
-	debugPrint(grid)
-	if debugging {
-		fmt.Println()
+	for i := 0; i < cycles; i++ {
+		tilted = tiltNorth(tilted)
+		tilted = tiltWest(tilted)
+		tilted = tiltSouth(tilted)
+		tilted = tiltEast(tilted)
+		load := calculateLoad(tilted)
+
+		state := gridToString(tilted)
+		matchIdx := -1
+		for j, h := range loadHistory {
+			if h.state == state {
+				matchIdx = j
+				break
+			}
+		}
+		if matchIdx > -1 {
+			remaining := cycles - i - 1
+			cycleSize := i - matchIdx
+			partTwoTotal = loadHistory[matchIdx+(remaining%cycleSize)].load
+			break
+		}
+		loadHistory = append(loadHistory, history{
+			load:  load,
+			state: gridToString(tilted),
+		})
+		if debugging {
+			fmt.Println(gridToString(tilted))
+			fmt.Println()
+		}
 	}
-	debugPrint(tilted)
-	fmt.Printf("Part 1: %d\n", partOneTotal)
+
+	fmt.Printf("Part 2: %d\n", partTwoTotal)
 }
 
-func tiltUp(grid [][]cellType) [][]cellType {
+func tiltNorth(grid [][]cellType) [][]cellType {
 	newGrid := make([][]cellType, len(grid))
 	cols := len(grid[0])
 	for col := 0; col < cols; col++ {
@@ -77,6 +110,98 @@ func tiltUp(grid [][]cellType) [][]cellType {
 	return newGrid
 }
 
+func tiltSouth(grid [][]cellType) [][]cellType {
+	newGrid := make([][]cellType, len(grid))
+	cols := len(grid[0])
+	for col := 0; col < cols; col++ {
+		for i := cols - 1; i >= 0; i-- {
+			if col == 0 {
+				newGrid[i] = make([]cellType, cols)
+			}
+			row := grid[i]
+			if row[col] == rock {
+				if i == cols-1 {
+					newGrid[i][col] = rock
+				} else {
+					for j := i + 1; j < cols; j++ {
+						if newGrid[j][col] != empty {
+							newGrid[j-1][col] = rock
+							break
+						} else if j == cols-1 {
+							newGrid[j][col] = rock
+						} else {
+							newGrid[j-1][col] = empty
+						}
+					}
+				}
+			} else {
+				newGrid[i][col] = row[col]
+			}
+		}
+	}
+	return newGrid
+}
+
+func tiltEast(grid [][]cellType) [][]cellType {
+	newGrid := make([][]cellType, len(grid))
+	cols := len(grid[0])
+	for i, row := range grid {
+		newGrid[i] = make([]cellType, cols)
+
+		for j := cols - 1; j >= 0; j-- {
+			if row[j] == rock {
+				if j == cols-1 {
+					newGrid[i][j] = rock
+				} else {
+					for k := j + 1; k < cols; k++ {
+						if newGrid[i][k] != empty {
+							newGrid[i][k-1] = rock
+							break
+						} else if k == cols-1 {
+							newGrid[i][k] = rock
+						} else {
+							newGrid[i][k-1] = empty
+						}
+					}
+				}
+			} else {
+				newGrid[i][j] = row[j]
+			}
+		}
+	}
+	return newGrid
+}
+
+func tiltWest(grid [][]cellType) [][]cellType {
+	newGrid := make([][]cellType, len(grid))
+	cols := len(grid[0])
+	for i, row := range grid {
+		newGrid[i] = make([]cellType, cols)
+
+		for j := 0; j < cols; j++ {
+			if row[j] == rock {
+				if j == 0 {
+					newGrid[i][j] = rock
+				} else {
+					for k := j - 1; k >= 0; k-- {
+						if newGrid[i][k] != empty {
+							newGrid[i][k+1] = rock
+							break
+						} else if k == 0 {
+							newGrid[i][k] = rock
+						} else {
+							newGrid[i][k+1] = empty
+						}
+					}
+				}
+			} else {
+				newGrid[i][j] = row[j]
+			}
+		}
+	}
+	return newGrid
+}
+
 func calculateLoad(grid [][]cellType) int {
 	total := 0
 	for i, row := range grid {
@@ -89,20 +214,19 @@ func calculateLoad(grid [][]cellType) int {
 	return total
 }
 
-func debugPrint(grid [][]cellType) {
-	if !debugging {
-		return
-	}
+func gridToString(grid [][]cellType) string {
+	str := ""
 	for _, row := range grid {
 		for _, cell := range row {
 			if cell == rock {
-				fmt.Print("O")
+				str += "O"
 			} else if cell == wall {
-				fmt.Print("#")
+				str += "#"
 			} else {
-				fmt.Print(".")
+				str += "."
 			}
 		}
-		fmt.Println()
+		str += "\n"
 	}
+	return str
 }
