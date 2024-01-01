5383d925beba58f1203d7edb934daa40d132b643
Peter Weightman <peter@peterweightman.com>
2024-01-01 20:49:54 +0000

    2023 day 23 golang part 2 (takes a few minutes to run)

--- 2023/23/go/main.go
+++ 2023/23/go/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	utils "peterweightman.com/aoc/utils"
 )
 
-var rows, cols int
+type day struct {
+	rows, cols    int
+	start, end    utils.Coords
+	grid          map[utils.Coords]string
+	collapsedGrid map[utils.Coords]map[utils.Coords]int
+}
 
 func main() {
+	start_time := time.Now()
+
 	var useExample bool
 	flag.BoolVar(&useExample, "use-example", false, "Use example. Default is false")
 	flag.Parse()
@@ -19,164 +27,130 @@ func main() {
 		fileName = "input_example.txt"
 	}
 
-	grid := make(map[utils.Coords]string)
+	d := day{
+		rows:          0,
+		grid:          make(map[utils.Coords]string),
+		collapsedGrid: make(map[utils.Coords]map[utils.Coords]int),
+	}
 
-	rows = 0
 	utils.IterateFileLines("../"+fileName, func(line string) {
 		for col, char := range line {
-			grid[utils.Coords{Row: rows, Col: col}] = string(char)
+			d.grid[utils.Coords{Row: d.rows, Col: col}] = string(char)
 		}
-		rows++
-		cols = len(line)
+		d.rows++
+		d.cols = len(line)
 	})
 
-	start := utils.Coords{Row: 0, Col: 1}
-	end := utils.Coords{Row: rows - 1, Col: cols - 2}
+	d.start = utils.Coords{Row: 0, Col: 1}
+	d.end = utils.Coords{Row: d.rows - 1, Col: d.cols - 2}
 
-	trail := make(map[utils.Coords]bool)
-	partOneTrail, ok := longestTrail(grid, trail, start, end)
-	// debugTrail(grid, partOneTrail, rows, cols)
+	d.collapsedGrid[d.start] = make(map[utils.Coords]int)
+	d.collapseGrid(d.start.Down(), d.start, d.start)
+
+	partTwoTotal, ok := d.solvePartTwo(0, d.start, make(map[utils.Coords]bool))
 	if !ok {
-		panic("Should be at least one trail")
+		panic("Couldn't find a path to the end")
 	}
-
-	fmt.Printf("Part 1: %d\n", len(partOneTrail))
+	fmt.Printf("Part 2: %d (%s)\n", partTwoTotal, time.Since(start_time))
 }
 
-func moveLeft(grid map[utils.Coords]string, trail map[utils.Coords]bool, pos utils.Coords) (newPos utils.Coords, ok bool) {
-	left := pos.Left()
-	if trail[pos] {
-		return left, false
-	} else if grid[pos] == "#" {
-		return left, false
-	} else if grid[pos] == ">" {
-		return left, false
+func (d day) canMoveTo(pos, prev utils.Coords) bool {
+	if pos.IsEqual(prev) {
+		return false
 	}
-	return left, true
+
+	cell, ok := d.grid[pos]
+	if cell == "#" || !ok {
+		return false
+	}
+	return true
 }
 
-func moveRight(grid map[utils.Coords]string, trail map[utils.Coords]bool, pos utils.Coords) (newPos utils.Coords, ok bool) {
-	right := pos.Right()
-	if trail[pos] {
-		return right, false
-	} else if grid[pos] == "#" {
-		return right, false
-	} else if grid[pos] == "<" {
-		return right, false
+func (d day) collapseGrid(currPos, prevPos, fromPos utils.Coords) {
+	steps := 0
+
+	for {
+		if currPos.IsEqual(d.end) {
+			d.collapsedGrid[fromPos][currPos] = steps + 1
+			return
+		}
+
+		above := currPos.Up()
+		right := currPos.Right()
+		below := currPos.Down()
+		left := currPos.Left()
+
+		possibleNext := make([]utils.Coords, 0)
+
+		if d.canMoveTo(above, prevPos) {
+			possibleNext = append(possibleNext, above)
+		}
+		if d.canMoveTo(right, prevPos) {
+			possibleNext = append(possibleNext, right)
+		}
+		if d.canMoveTo(below, prevPos) {
+			possibleNext = append(possibleNext, below)
+		}
+		if d.canMoveTo(left, prevPos) {
+			possibleNext = append(possibleNext, left)
+		}
+
+		if len(possibleNext) == 0 {
+			// dead end? -> ignore
+			return
+		} else if len(possibleNext) == 1 {
+			steps++
+			prevPos = currPos
+			currPos = possibleNext[0]
+		} else {
+			d.collapsedGrid[fromPos][currPos] = steps + 1
+			_, hasNext := d.collapsedGrid[currPos]
+			if hasNext {
+				d.collapsedGrid[currPos][fromPos] = steps + 1
+				return
+			} else {
+				d.collapsedGrid[currPos] = map[utils.Coords]int{
+					fromPos: steps + 1,
+				}
+			}
+			for _, pos := range possibleNext {
+				d.collapseGrid(pos, currPos, currPos)
+			}
+			return
+		}
 	}
-	return right, true
 }
 
-func moveUp(grid map[utils.Coords]string, trail map[utils.Coords]bool, pos utils.Coords) (newPos utils.Coords, ok bool) {
-	up := pos.Up()
-	if trail[pos] {
-		return up, false
-	} else if grid[pos] == "#" {
-		return up, false
-	} else if grid[pos] == "v" {
-		return up, false
-	}
-	return up, true
-}
-
-func moveDown(grid map[utils.Coords]string, trail map[utils.Coords]bool, pos utils.Coords) (newPos utils.Coords, ok bool) {
-	down := pos.Down()
-	if trail[pos] {
-		return down, false
-	} else if grid[pos] == "#" {
-		return down, false
-	} else if grid[pos] == "^" {
-		return down, false
-	}
-	return down, true
-}
-
-func cloneTrail(trail map[utils.Coords]bool) map[utils.Coords]bool {
+func cloneVisited(toCopy map[utils.Coords]bool) map[utils.Coords]bool {
 	cloned := make(map[utils.Coords]bool)
-	for k, v := range trail {
+	for k, v := range toCopy {
 		cloned[k] = v
 	}
 	return cloned
 }
 
-func longestTrail(grid map[utils.Coords]string, trail map[utils.Coords]bool, pos, end utils.Coords) (map[utils.Coords]bool, bool) {
-	clonedTrail := cloneTrail(trail)
-	clonedTrail[pos] = true
-	if pos.IsEqual(end) {
-		return trail, true
+func (d day) solvePartTwo(steps int, curr utils.Coords, visited map[utils.Coords]bool) (totalSteps int, ok bool) {
+	if curr.IsEqual(d.end) {
+		return steps, true
 	}
 
-	trails := make([]map[utils.Coords]bool, 0)
+	if visited[curr] {
+		return steps, false
+	}
+	visited[curr] = true
 
-	if grid[pos] == "." || grid[pos] == "^" {
-		above, ok := moveUp(grid, trail, pos)
-		if ok {
-			aboveTrail, ok := longestTrail(grid, clonedTrail, above, end)
-			if ok {
-				trails = append(trails, aboveTrail)
-			}
+	next, ok := d.collapsedGrid[curr]
+	if !ok {
+		return steps, false
+	}
+
+	best := -1
+	for nextPos, nextSteps := range next {
+		stepsFromPath, ok := d.solvePartTwo(steps+nextSteps, nextPos, cloneVisited(visited))
+		if ok && stepsFromPath > best {
+			best = stepsFromPath
 		}
 	}
 
-	if grid[pos] == "." || grid[pos] == ">" {
-		right, ok := moveRight(grid, trail, pos)
-		if ok {
-			rightTrail, ok := longestTrail(grid, clonedTrail, right, end)
-
-			if ok {
-				trails = append(trails, rightTrail)
-			}
-		}
-	}
-
-	if grid[pos] == "." || grid[pos] == "v" {
-		below, ok := moveDown(grid, trail, pos)
-		if ok {
-			belowTrail, ok := longestTrail(grid, clonedTrail, below, end)
-			if ok {
-				trails = append(trails, belowTrail)
-			}
-		}
-	}
-
-	if grid[pos] == "." || grid[pos] == "<" {
-		left, ok := moveLeft(grid, trail, pos)
-		if ok {
-			leftTrail, ok := longestTrail(grid, clonedTrail, left, end)
-			if ok {
-				trails = append(trails, leftTrail)
-			}
-		}
-	}
-
-	if len(trails) == 0 {
-		return nil, false
-	}
-
-	longestCount := 0
-	var longest map[utils.Coords]bool
-	for _, t := range trails {
-		if len(t) > longestCount {
-			longest = t
-			longestCount = len(t)
-		}
-	}
-
-	return longest, true
-}
-
-func debugTrail(grid map[utils.Coords]string, trail map[utils.Coords]bool, rows, cols int) {
-	for i := 0; i < rows; i++ {
-		line := ""
-		for j := 0; j < cols; j++ {
-			pos := utils.Coords{Row: i, Col: j}
-			if trail[pos] {
-				line += "O"
-			} else {
-				line += grid[pos]
-			}
-		}
-		fmt.Println(line)
-	}
-	fmt.Println()
+	return best, best > -1
 }
