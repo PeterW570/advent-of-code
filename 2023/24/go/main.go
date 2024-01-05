2bca7a597979b22b4ccb4534a6b40dcd38ed9819
Peter Weightman <peter@peterweightman.com>
2024-01-05 22:12:57 +0000

    2023 day 24 golang part 2 (maths copied from someone else's solution)
    https://www.reddit.com/r/adventofcode/comments/18pum3b/2023_day_24_part_2_does_anyone_have_an_algebraic/kes32ef/

--- 2023/24/go/main.go
+++ 2023/24/go/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"strconv"
 	"strings"
@@ -14,10 +13,6 @@ type Vector3 struct {
 	x, y, z float64
 }
 
-type Vector2 struct {
-	x, y float64
-}
-
 type hailstone struct {
 	pos, vel Vector3
 }
@@ -25,58 +20,25 @@ type hailstone struct {
 func main() {
 	startTime := time.Now()
 
-	var useExample bool
-	flag.BoolVar(&useExample, "use-example", false, "Use example. Default is false")
-	flag.Parse()
-
 	fileName := "input.txt"
-	min := float64(200000000000000)
-	max := float64(400000000000000)
-	if useExample {
-		fileName = "input_example.txt"
-		min = float64(7)
-		max = float64(27)
-	}
-
 	hailstones := make([]hailstone, 0)
 
 	utils.IterateFileLines("../"+fileName, func(line string) {
-		hailstones = append(hailstones, parseHailstone(line))
+		stone := parseHailstone(line)
+		hailstones = append(hailstones, stone)
 	})
 
-	partOneTotal := 0
-	for i := 0; i < len(hailstones)-1; i++ {
-		for j := i + 1; j < len(hailstones); j++ {
-			a, b := hailstones[i], hailstones[j]
-			if point, err := findIntersection(a, b); err == nil {
-				if !pointIsInTestArea(point.x, min, max) {
-					continue
-				} else if !pointIsInTestArea(point.y, min, max) {
-					continue
-				} else if !pointIsInFuture(point.x, a.pos.x, a.vel.x) {
-					continue
-				} else if !pointIsInFuture(point.y, a.pos.y, a.vel.y) {
-					continue
-				} else if !pointIsInFuture(point.x, b.pos.x, b.vel.x) {
-					continue
-				} else if !pointIsInFuture(point.y, b.pos.y, b.vel.y) {
-					continue
-				}
-				partOneTotal++
-			}
-		}
-	}
+	t1 := solveT1(hailstones)
+	t2 := solveT1([]hailstone{hailstones[1], hailstones[0], hailstones[2]})
 
-	fmt.Printf("Part 1: %d (%s)\n", partOneTotal, time.Since(startTime))
-}
+	c1 := calculateCollisionPosition(hailstones[0], t1)
+	c2 := calculateCollisionPosition(hailstones[1], t2)
+	v := calculateVelocity(c1, c2, t1, t2)
+	p := calculateFinalPosition(hailstones[0], v, t1)
 
-func pointIsInTestArea(point, min, max float64) bool {
-	return point >= min && point <= max
-}
+	partTwoTotal := sumSlice(p)
 
-func pointIsInFuture(point, initialPos, vel float64) bool {
-	posDiff := point - initialPos
-	return (posDiff > 0) == (vel > 0)
+	fmt.Printf("Part 2: %d (%s)\n", int(partTwoTotal), time.Since(startTime))
 }
 
 // px py pz @ vx vy vz
@@ -98,20 +60,51 @@ func parseHailstone(line string) hailstone {
 	}
 }
 
-func vectorCross(a, b Vector2) float64 {
-	return (a.x * b.y) - (a.y * b.x)
+// maths copied from solution: https://www.reddit.com/r/adventofcode/comments/18pum3b/2023_day_24_part_2_does_anyone_have_an_algebraic/kes32ef/
+func solveT1(rocks []hailstone) float64 {
+	r1, r2, r3 := rocks[0].pos, rocks[1].pos, rocks[2].pos
+	v1, v2, v3 := rocks[0].vel, rocks[1].vel, rocks[2].vel
+
+	yz := r1.y*(r2.z-r3.z) + r2.y*(-r1.z+r3.z) + r3.y*(r1.z-r2.z)
+	xz := r1.x*(-r2.z+r3.z) + r2.x*(r1.z-r3.z) + r3.x*(-r1.z+r2.z)
+	xy := r1.x*(r2.y-r3.y) + r2.x*(-r1.y+r3.y) + r3.x*(r1.y-r2.y)
+	vxvy := v1.x*(v2.y-v3.y) + v2.x*(-v1.y+v3.y) + v3.x*(v1.y-v2.y)
+	vxvz := v1.x*(-v2.z+v3.z) + v2.x*(v1.z-v3.z) + v3.x*(-v1.z+v2.z)
+	vyvz := v1.y*(v2.z-v3.z) + v2.y*(-v1.z+v3.z) + v3.y*(v1.z-v2.z)
+
+	n := (v2.x-v3.x)*yz + (v2.y-v3.y)*xz + (v2.z-v3.z)*xy
+	d := (r2.z-r3.z)*vxvy + (r2.y-r3.y)*vxvz + (r2.x-r3.x)*vyvz
+
+	return float64(n) / float64(d)
 }
 
-func findIntersection(a, b hailstone) (Vector2, error) {
-	a2 := Vector2{a.vel.x, a.vel.y}
-	b2 := Vector2{b.vel.x, b.vel.y}
-	d2 := Vector2{b.pos.x - a.pos.x, b.pos.y - a.pos.y}
-
-	det := vectorCross(a2, b2)
-	if det == 0 {
-		return Vector2{-1, -1}, fmt.Errorf("hailstones are parallel")
+func calculateCollisionPosition(rock hailstone, t float64) Vector3 {
+	collisionPosition := Vector3{
+		rock.pos.x + rock.vel.x*t,
+		rock.pos.y + rock.vel.y*t,
+		rock.pos.z + rock.vel.z*t,
 	}
+	return collisionPosition
+}
 
-	u := vectorCross(d2, b2) / det
-	return Vector2{a.pos.x + a.vel.x*u, a.pos.y + a.vel.y*u}, nil
+func calculateVelocity(c1, c2 Vector3, t1, t2 float64) Vector3 {
+	velocity := Vector3{
+		(c2.x - c1.x) / (t2 - t1),
+		(c2.y - c1.y) / (t2 - t1),
+		(c2.z - c1.z) / (t2 - t1),
+	}
+	return velocity
+}
+
+func calculateFinalPosition(rock hailstone, v Vector3, t float64) Vector3 {
+	finalPosition := Vector3{
+		rock.pos.x + rock.vel.x*t - v.x*t,
+		rock.pos.y + rock.vel.y*t - v.y*t,
+		rock.pos.z + rock.vel.z*t - v.z*t,
+	}
+	return finalPosition
+}
+
+func sumSlice(v Vector3) float64 {
+	return v.x + v.y + v.z
 }
