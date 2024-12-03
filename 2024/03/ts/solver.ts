b74102662b21d0167c0616b7d28d6fef163f0a6a
Peter Weightman <peter@peterweightman.com>
2024-12-03 17:38:38 +0000

    2024 day 03 part 2 deno

--- 2024/03/ts/solver.ts
+++ 2024/03/ts/solver.ts
@@ -1,9 +1,12 @@
 export function solve(input: string): number {
 	let sum = 0;
 
-	const matches = input.matchAll(/mul\((\d+),(\d+)\)/gm);
+	const matches = input.matchAll(/(?:mul\((\d+),(\d+)\))|(?:do\(\))|(?:don't\(\))/gm);
+	let enabled = true;
 	for (const match of matches) {
-		sum += parseInt(match[1]) * parseInt(match[2]);
+		if (match[0] === "do()") enabled = true;
+		else if (match[0] === "don't()") enabled = false;
+		else if (enabled) sum += parseInt(match[1]) * parseInt(match[2]);
 	}
 	return sum;
 }
