ef14a85600591001756d1a7cbfd5d77e5d8bce3b
Peter Weightman <peter@peterweightman.com>
2024-12-05 13:11:09 +0000

    2024 day 05 part 1 deno

---
+++ 2024/05/ts/solver.ts
@@ -0,0 +1,35 @@
+export function solve(input: string): number {
+	let total = 0;
+
+	const lines = input.split("\n");
+	const rules: [string, string][] = [];
+	const pages: string[][] = [];
+
+	for (const line of lines) {
+		if (line.includes("|")) {
+			const splits = line.split("|");
+			rules.push([splits[0], splits[1]]);
+		} else if (line.length) {
+			pages.push(line.split(","));
+		}
+	}
+
+	for (const page of pages) {
+		let valid = true;
+
+		for (const rule of rules) {
+			const aIdx = page.findIndex((x) => x === rule[0]);
+			const bIdx = page.findIndex((x) => x === rule[1]);
+			if (aIdx > -1 && bIdx > -1 && bIdx < aIdx) {
+				valid = false;
+				break;
+			}
+		}
+
+		if (valid) {
+			total += parseInt(page[Math.floor(page.length / 2)]);
+		}
+	}
+
+	return total;
+}
