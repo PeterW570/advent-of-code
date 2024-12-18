4a516aa764b4c517eab8d1a5e811c890a1d10c77
Peter Weightman <peter@peterweightman.com>
2024-12-18 18:42:47 +0000

    2024 day 18 part 1 deno

---
+++ 2024/18/ts/solver.ts
@@ -0,0 +1,170 @@
+interface Pos {
+	row: number;
+	col: number;
+}
+const posToStr = ({ row, col }: Pos) => `${row},${col}`;
+const strToPos = (str: string) => {
+	const [rowStr, colStr] = str.split(",");
+	return {
+		row: parseInt(rowStr),
+		col: parseInt(colStr),
+	};
+};
+
+interface PuzzleState {
+	size: number;
+	corruptedMap: Partial<Record<number, Partial<Record<number, boolean>>>>;
+	start: Pos;
+	end: Pos;
+}
+
+const dirs = [
+	[-1, 0],
+	[0, 1],
+	[1, 0],
+	[0, -1],
+];
+
+class Queue {
+	posStrs: Set<string>;
+	scoreForPos: (pos: Pos) => number;
+
+	constructor(startPos: Pos, h: (pos: Pos) => number) {
+		this.posStrs = new Set<string>();
+		this.scoreForPos = h;
+
+		this.enqueue(startPos);
+	}
+
+	enqueue(pos: Pos) {
+		const posStr = posToStr(pos);
+		this.posStrs.add(posStr);
+	}
+
+	dequeue() {
+		const sorted = Array.from(this.posStrs)
+			.map((pos) => ({ pos, score: this.scoreForPos(strToPos(pos)) }))
+			.sort((a, b) => a.score - b.score);
+
+		const firstPosStr = sorted[0]?.pos;
+
+		if (firstPosStr) {
+			this.posStrs.delete(firstPosStr);
+		}
+
+		return strToPos(firstPosStr);
+	}
+
+	has(pos: Pos) {
+		return this.posStrs.has(posToStr(pos));
+	}
+
+	any() {
+		return this.posStrs.size > 0;
+	}
+}
+
+// function reconstruct_path(cameFrom, current)
+//     total_path := {current}
+//     while current in cameFrom.Keys:
+//         current := cameFrom[current]
+//         total_path.prepend(current)
+//     return total_path
+function reconstructPath(from: Map<string, string>, current: Pos) {
+	let currentStr = posToStr(current);
+	const path = [currentStr];
+	while (from.has(currentStr)) {
+		currentStr = from.get(currentStr)!;
+		path.unshift(currentStr);
+	}
+	return path;
+}
+
+function aStar({ start, end, size, corruptedMap }: PuzzleState) {
+	// For node n, gScore[n] is the currently known cost of the cheapest path from start to n.
+	const gScore = new Map<string, number>();
+	gScore.set(posToStr(start), 0);
+
+	const h = (pos: Pos) => end.row - pos.row + end.col - pos.col;
+
+	// For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
+	// how cheap a path could be from start to finish if it goes through n.
+	const fScore = new Map<string, number>();
+	fScore.set(posToStr(start), h(start));
+
+	const openSet = new Queue(start, (pos) => fScore.get(posToStr(pos)) ?? Infinity);
+	openSet.enqueue(start);
+
+	const from: Map<string, string> = new Map();
+
+	while (openSet.any()) {
+		const current = openSet.dequeue()!;
+		if (posToStr(current) === posToStr(end)) {
+			return reconstructPath(from, current);
+		}
+
+		for (const dir of dirs) {
+			const nextPos = {
+				row: current.row + dir[0],
+				col: current.col + dir[1],
+			};
+			if (nextPos.row < 0 || nextPos.row > size || nextPos.col < 0 || nextPos.col > size) {
+				continue;
+			}
+			if (corruptedMap[nextPos.row]?.[nextPos.col]) {
+				continue;
+			}
+			const nextPosStr = posToStr(nextPos);
+
+			const tempGScore = gScore.get(posToStr(current))! + 1;
+			if (tempGScore < (gScore.get(nextPosStr) ?? Infinity)) {
+				from.set(nextPosStr, posToStr(current));
+				gScore.set(nextPosStr, tempGScore);
+				fScore.set(nextPosStr, tempGScore + h(nextPos));
+				openSet.enqueue(nextPos);
+			}
+		}
+	}
+
+	throw new Error("Couldn't find path from start to end");
+}
+
+export function solve(input: string, memorySize = 70, bytes = 1024): number {
+	const lines = input.split("\n");
+
+	const puzzleState: PuzzleState = {
+		size: memorySize,
+		corruptedMap: {},
+		start: { row: 0, col: 0 },
+		end: { row: memorySize, col: memorySize },
+	};
+
+	for (let i = 0; i < bytes && i < lines.length; i++) {
+		const [colStr, rowStr] = lines[i].split(",");
+		const row = parseInt(rowStr);
+		const col = parseInt(colStr);
+		if (!puzzleState.corruptedMap[row]) {
+			puzzleState.corruptedMap[row] = {};
+		}
+		puzzleState.corruptedMap[row][col] = true;
+	}
+
+	// _printMap(puzzleState);
+
+	const bestPathToEnd = aStar(puzzleState);
+	return bestPathToEnd.length - 1; // don't include start position
+}
+
+function _printMap({ size, corruptedMap }: PuzzleState) {
+	for (let row = 0; row <= size; row++) {
+		let rowStr = "";
+		for (let col = 0; col <= size; col++) {
+			if (corruptedMap[row]?.[col]) {
+				rowStr += "#";
+			} else {
+				rowStr += ".";
+			}
+		}
+		console.log(rowStr);
+	}
+}
---
+++ 2024/18/ts/test.ts
@@ -0,0 +1,42 @@
+import { assertEquals } from "jsr:@std/assert";
+
+import { solve } from "./solver.ts";
+
+const testCases = [
+	{
+		input: `5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0`,
+		memorySize: 6,
+		bytes: 12,
+		expected: 22,
+	},
+];
+
+Deno.test("day 18", () => {
+	for (const { input, memorySize, bytes, expected } of testCases) {
+		assertEquals(solve(input, memorySize, bytes), expected);
+	}
+});
