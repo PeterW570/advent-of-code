9644529ca24b8cb3f89d301b3f451dc283bcf082
Peter Weightman <peter@peterweightman.com>
2025-12-05 12:44:39 +0000

    2025 day 05 part 2 swift

--- 2025/05/Sources/05/Solver.swift
+++ 2025/05/Sources/05/Solver.swift
@@ -4,18 +4,32 @@ public func solve(input: String) -> String {
     let lines = input.split(whereSeparator: \.isNewline).map(String.init)
     var solution = 0
 
-    var freshRanges: [ClosedRange<Int>] = []
+    var distinctRanges: [ClosedRange<Int>] = []
     for line in lines {
-        if line.contains("-") {
-            let rangeBounds = line.split(separator: "-").map { Int($0)! }
-            let range = rangeBounds[0]...rangeBounds[1]
-            freshRanges.append(range)
-        } else {
-            let ingredientId = Int(line)!
-            if freshRanges.contains(where: { $0.contains(ingredientId) }) {
-                solution += 1
+        if !line.contains("-") {
+            break
+        }
+
+        let rangeBounds = line.split(separator: "-").map { Int($0)! }
+        let lowerBound = rangeBounds[0]
+        let upperBound = rangeBounds[1]
+        var range = lowerBound...upperBound
+
+        for existingRange in distinctRanges {
+            if existingRange.overlaps(range) {
+                let merged =
+                    min(
+                        existingRange.lowerBound, range.lowerBound)...max(
+                        existingRange.upperBound, range.upperBound)
+                distinctRanges.removeAll { $0 == existingRange }
+                range = merged
             }
         }
+        distinctRanges.append(range)
+    }
+
+    for range in distinctRanges {
+        solution += range.count
     }
 
     return "Solution: \(solution)"
